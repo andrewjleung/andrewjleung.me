3d6f886fef968f26640cd714cc2c152a02125c3e
Andrew Leung <andrewleung104@gmail.com>
2022-05-25 22:17:11 -0400

    remove spotify iframe comment

--- pages/index.js
+++ pages/index.js
@@ -60,19 +60,9 @@ export default function Home() {
                 . Take a listen!
               </p>
             </div>
-            {/* <iframe
-              style="border-radius: 12px"
-              src="https://open.spotify.com/embed/artist/00zDjeTQDVOFlNttOnv9bc?utm_source=generator"
-              width="100%"
-              height="250"
-              frameborder="0"
-              allowfullscreen=""
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            ></iframe> */}
           </div>
         </div>
       </main>
-
       <footer className={styles.footer}>🚧 More website coming soon...</footer>
     </div>
   );
