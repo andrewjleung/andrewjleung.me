0fb8c5c2c2520a717cf5b6332fa2f83da009f02d
Andrew Leung <andrewleung104@gmail.com>
2022-08-26 02:59:16 -0400

    Make blog look slightly nicer

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,16 +3,16 @@ import Container from "../../components/Container";
 export default function Blog() {
   return (
     <Container>
-      <h1 className="pl-3">Blog</h1>
+      <h1 className="pl-6">Blog</h1>
       <a
-        className="w-fit px-3 no-underline rounded-xl hover:shadow-lg duration-200 ease-out active:shadow-none"
+        className="w-fit px-6 no-underline rounded-xl hover:shadow-lg duration-200 ease-out active:shadow-none"
         href="./blog/quitters"
       >
         <h3>20220826 - Quitters</h3>
         <p>
-          A roadtrip through my past and current perspectives on making music,
-          finding meaning in that, and losing meaning in that. Slight DAW
-          hatred.
+          A roadtrip through my past and present perspectives on making music,
+          being a musician, finding meaning in that, and losing meaning in that.
+          Undertones of DAW hatred.
         </p>
       </a>
     </Container>
